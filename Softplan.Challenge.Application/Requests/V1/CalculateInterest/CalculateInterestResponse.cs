9582d2b812de83b24fb700a47663cd21d53530f7
Heverson Ribeiro Resende <heverson.resende@wappa.com.br>
2021-04-04 15:09:49 -0300

    Setting value response to decimal

--- Softplan.Challenge.Application/Requests/V1/CalculateInterest/CalculateInterestResponse.cs
+++ Softplan.Challenge.Application/Requests/V1/CalculateInterest/CalculateInterestResponse.cs
@@ -1,18 +1,15 @@
-﻿
-using System.Globalization;
-
-namespace Softplan.Challenge.Application.Requests.V1.CalculateInterest
+﻿namespace Softplan.Challenge.Application.Requests.V1.CalculateInterest
 {
     public class CalculateInterestResponse
     {
         /// <summary>
         /// The amount of the interest calculation.
         /// </summary>
-        public string Value { get; set; }
+        public decimal Value { get; set; }
 
         public CalculateInterestResponse(decimal value)
         {
-            Value = value.ToString("F", new CultureInfo("pt-BR").NumberFormat); 
+            Value = value; 
         }
 
     }
